23bd69cbe9fac0959f997ef049b29bcc6fb4127c
kawaegle <alecromski@tutanota.com>
2022-01-23 19:52:21 +0100

    add the selector of the mode

--- client/include/client.h
+++ client/include/client.h
@@ -1,4 +1,16 @@
 #ifndef _CLIENT_H_
     #define _CLIENT_H_
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+void helper(void);
+
+int set_mode(int mode);
+
+int white(void);
+
+int random_data(void);
+
+int random_seed(void);
 #endif
