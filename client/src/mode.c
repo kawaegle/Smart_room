7d4891df32df77f59741bfd5aa3309b7e1ce2982
kawaegle <alecromski@tutanota.com>
2022-01-31 18:20:11 +0100

    define clients mode

--- client/src/mode.c
+++ client/src/mode.c
@@ -1,14 +1,18 @@
 #include "../include/client.h"
-#include <stdio.h>
 
 int set_mode(int mode)
 {
-    if (mode == 1)
+    if (mode == 1) {
         white();
-    if (mode == 2)
+    }
+
+    if (mode == 2) {
         random_data();
-    if (mode == 3)
+    }
+
+    if (mode == 3) {
         random_seed();
+    }
 
     return (0);
 }
